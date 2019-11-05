71aa35bcb2b96871c3b4d6ec61e5cc8802eedf0d
anupamkhera <anupamkhera.97@gmail.com>
2019-11-05 17:49:19 +0530

    listings, realtors models created

--- listings/views.py
+++ listings/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+listings = Listing.objects.all()
+
+context = {
+    'listings' : listings
+}
 def index(request):
-    return render(request, 'listings/listings.html')
+    return render(request, 'listings/listings.html',context)
 
 def listing(request):
     return render(request, 'listings/listing.html')
